dfbfc22bc5bb8b44da8b8e5c0c34ef63beb56059
rodin <rodin@Siri-Lenovo.dhcp.wustl.edu>
2024-10-02 15:55:06 -0500

    erirnnn

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,38 @@
 package studio4;
 
 import java.awt.Color;
+import java.util.Scanner;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		Color pink2 = new Color(239, 142, 154);
+		StdDraw.setPenColor(pink2);
+		StdDraw.filledRectangle(11, 8, 11, 8);
 		
+		Color hair = new Color(245, 124, 0);
+		StdDraw.setPenColor(hair);
+		StdDraw.filledEllipse(0.5, 0.54, 0.229, 0.23);
+		StdDraw.filledRectangle(.5, .3, .23, .25);
+
+		
+		Color skin = new Color(255, 224, 178);
+		StdDraw.setPenColor(skin);
+		StdDraw.filledEllipse(0.5, 0.5, 0.2, 0.23);
+		 
+		Color eye = new Color(174, 213, 129);
+		StdDraw.setPenColor(eye);
+		StdDraw.filledEllipse(0.42, 0.53, 0.02, 0.04);
+		StdDraw.filledEllipse(0.57, 0.53, 0.02, 0.04);
+		
+		
+		Color mouth = new Color(255, 127, 127);
+		StdDraw.setPenColor(mouth);
+		StdDraw.filledEllipse(0.5, 0.39, 0.09, 0.04);
+		
+		StdDraw.setPenColor(skin);
+		StdDraw.filledEllipse(0.5, 0.4, 0.09, 0.04);
+	;
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -15,10 +15,47 @@ import edu.princeton.cs.introcs.StdDraw;
 public class InterpretDrawingFile {
 
 	public static void main(String[] args) throws FileNotFoundException {
+
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		StdDraw.setPenColor(red, green, blue);
+
+
+		if (shape.equals ("rectangle")) { //can't use == for string, have to methods it eg .equals
+			double X = in.nextDouble();
+			double Y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			StdDraw.rectangle(X, Y, halfWidth, halfHeight);
+
+		}
+
+		if (shape.equals ("triangle")) {
+			double [] x = new double [3];
+			double [] y = new double [3];		
+			x[0] = in.nextDouble();
+			y[0] = in.nextDouble();
+			x[1] = in.nextDouble();
+			y[1] = in.nextDouble();
+			x[2] = in.nextDouble();
+			y[2] = in.nextDouble();
+			if (!filled) {
+				StdDraw.polygon(x, y);
+			}
+			else {
+				StdDraw.filledPolygon(x, y);
+			}
+		}
+
+
+
+
 	}
 }
